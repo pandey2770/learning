a01d5b065a355b086e923e7582ef481de2b2872a
jpuri <jyotipuri@gmail.com>
2018-01-07 15:28:59 +0530

    Login integration

--- client/src/Main.js
+++ client/src/Main.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 import  './Css.css';
 
 
 class App extends Component {
+  state = {
+    user: undefined
+  }
+
+  async componentWillMount() {
+    const { data: user } = await axios.get('/api/currentuser');
+    this.setState({
+      user
+    });
+  }
+
   render() {
+    const { user } = this.state;
+    console.log('********', user)
     return (
       <div>
         <nav className="navbar navbar-expand navbar-light bg-light">
@@ -15,6 +29,8 @@ class App extends Component {
                 <Link to={`/Home`}><li className="nav-item nav-link active">Home <span className="sr-only">(current)</span></li></Link>
                 <Link to={`/Two`}><li className="nav-item nav-link" >Features</li></Link>
                 <Link to={`/Three`}><li className="nav-item nav-link">Pricing</li></Link>
+                {user ? <Link to={`/Logout`}><li className="nav-item nav-link">Logout</li></Link>:
+                  <Link to={`/Login`}><li className="nav-item nav-link">Login</li></Link>}
                 <Link to={`/Cart`}><li className="nav-item nav-link">Cart</li></Link>
               </ul>  
             </div>
