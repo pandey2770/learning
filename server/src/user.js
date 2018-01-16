06c4a8fa680d369075c47669272ca38f9e4b5d7f
unknown <pandey.abhishek2770@gmail.com>
2018-01-16 11:34:34 +0530

    changeDb name issue

--- server/src/user.js
+++ server/src/user.js
@@ -1,10 +1,10 @@
 const uuidv1 = require("uuid/v1");
 const DB = require("./db");
 
-async function get(username, password) {
+async function get(email, password) {
   const query = {
-    text: "SELECT * FROM demouser WHERE username = $1 and password = $2 ",
-    values: [username, password]
+    text: "SELECT * FROM demouser WHERE email = $1 and password = $2 ",
+    values: [email, password]
   };
   const users = await DB.get(query);
   return users;
@@ -18,11 +18,11 @@ async function getById(id) {
   return await DB.get(query);
 }
 
-async function signUp(username, password) {
+async function signUp(email, password) {
   const id = uuidv1();
   const query = {
-    text: "INSERT INTO demouser (id, username, password) VALUES ($1, $2, $3)",
-    values: [id, username, password]
+    text: "INSERT INTO demouser (id, email, password) VALUES ($1, $2, $3)",
+    values: [id, email, password]
   };
   await DB.mutate(query);
   return id;
@@ -30,8 +30,8 @@ async function signUp(username, password) {
 
 async function updateData (id,user ) {
   const query = {
-    text: "UPDATE demouser SET username = $2, password = $3, name = $4, number = $5 , address = $6  where id = $1",
-    values: [id, user.username, user.password, user.name, user.number, user.address],
+    text: "UPDATE demouser SET email = $2, password = $3, name = $4, number = $5 , address = $6  where id = $1",
+    values: [id, user.email, user.password, user.name, user.number, user.address],
   };
   return await DB.mutate(query);
 }
